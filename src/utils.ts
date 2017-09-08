02df1c89d9e4381fc20301a3a9f98c7f40fe18ab
Andrew Fong <id@andrewfong.com>
2017-09-07 18:37:05 -0700

    New meta helpers (lets us use "." syntax)

--- src/utils.ts
+++ src/utils.ts
@@ -21,3 +21,35 @@ const randomString = (numChars: number) => {
 export const idFn = (action: Action) => {
   return action.type + "#"  + randomString(7);
 };
+
+// For meta functions below
+const separator = ".";
+
+/*
+  Returns key on object, treating separator in key as sub-accessors
+*/
+export const getMeta = (obj: any, key: string): any => {
+  let current = obj;
+  let parts = key.split(separator);
+  for (let key in parts) {
+    if (! current) return undefined;
+    current = current[key];
+  }
+  return current;
+};
+
+/*
+  Helper function sets a key on object,
+  treating separator in key as sub-accessors
+*/
+export const setMeta = (obj: any, key: string, val: any) => {
+  let ret = { ...obj };
+  let parent = ret;
+  let parts = key.split(separator);
+  parts.slice(0, -1).forEach(k => {
+    parent[k] = { ...parent[k] };
+    parent = parent[k];
+  });
+  parent[parts[parts.length - 1]] = val;
+  return ret;
+};
