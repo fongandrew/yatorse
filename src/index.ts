ee104903303df9ffd6acd02d6bab2b777100f850
Andrew Fong <id@andrewfong.com>
2017-09-07 18:50:10 -0700

    Update index references

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
-export {
-  Config,
-  Hooks,
-  Proc,
-  PutAction
-} from "./types";
+export { Config } from "./config";
+export { Domain, BaseInterface, Handler } from "./domains";
 
 import enhancerFactory from "./enhancer";
 export default enhancerFactory;
